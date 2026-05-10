09dc7218c4f677e80560c1abf6b5c2b0120f4bc7
Shaedow2000 <shaedow2000@proton.me>
2026-05-10 14:43:09 +0100

    Updated the model

--- src/model/id.ts
+++ src/model/id.ts
@@ -1,30 +1,43 @@
 import * as mongoose from "mongoose";
 import validator from "validator";
 
-const IdSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: [true, "A username is required"],
-    unique: [true, "This username is already in use"],
+const IdSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "A username is required"],
+      trim: true,
+      unique: [true, "This username is already in use"],
+    },
+    email: {
+      type: String,
+      required: [true, "An email is required"],
+      unique: [true, "This email is already in use"],
+      validate: [validator.isEmail, "Invalid email"],
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: [true, "A password is required"],
+      minLength: [6, "A password should be 6 characters or more"],
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    verificationCode: { type: String },
+    verificationExpiry: { type: Date },
+    resetCode: { type: String, default: null },
+    resetExpiry: { type: Date, default: null },
+    refreshToken: [
+      {
+        token: { type: String },
+        expiry: { type: Date },
+      },
+    ],
   },
-  email: {
-    type: String,
-    required: [true, "An email is required"],
-    unique: [true, "This email is already in use"],
-    validate: [validator.isEmail, "Invalid email"],
-  },
-  password: {
-    type: String,
-    required: [true, "A password is required"],
-    minLength: [6, "A password should be 6 characters or more"],
-  },
-  isVerified: {
-    type: Boolean,
-    default: false,
-  },
-  verificationCode: { type: String },
-  verificationExpiry: { type: Date },
-});
+  { timestaps: true },
+);
 
 const IdModel = mongoose.model("Id", IdSchema);
 
