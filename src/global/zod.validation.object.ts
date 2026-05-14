eb9fb3ddfd3e5f2b2997961a10f5ae95d7e9e553
Shaedow2000 <shaedow2000@proton.me>
2026-05-14 21:48:58 +0100

    Add custom error messages to zod validation.

--- src/global/zod.validation.object.ts
+++ src/global/zod.validation.object.ts
@@ -1,10 +1,13 @@
 import * as z from "zod";
 
 const AccountZodObject = z.object({
-  username: z.string().trim().min(4).optional(),
-  email: z.email().trim().toLowerCase(),
-  password: z.string().min(6),
+  username: z
+    .string()
+    .trim()
+    .min(4, "Username too short (4 or more characters)")
+    .optional(),
+  email: z.email("Invalid email address").trim().toLowerCase(),
+  password: z.string().min(6, "Password too short (6 or more characters)"),
 });
 
 export default AccountZodObject;
-
